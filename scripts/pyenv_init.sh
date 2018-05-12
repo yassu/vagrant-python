c53f6656fd8ec21963f24fb791f6f91f0ad14b40
yassu <yassu0320.dev@gmail.com>
2018-05-12 15:22:26 +0900

    Add pyenv setting

--- scripts/pyenv_init.sh
+++ scripts/pyenv_init.sh
@@ -5,6 +5,7 @@ cd ~/.pyenv/plugins
 git clone git://github.com/yyuu/pyenv-virtualenv.git
 pyenv install 3.6.3
 pyenv global 3.6.3
+source ~/.bashrc
 
 pip install --upgrade pip
 if [ -f /vagrant/scripts/requirements.txt ]; then
