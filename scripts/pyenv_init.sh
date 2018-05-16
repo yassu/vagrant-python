301906332640a6c8f085c89d29be07f07d288b27
yassu <yassu0320.dev@gmail.com>
2018-05-17 00:19:42 +0900

    :heavy_plus_sign: Update Python Ver: to 3.6.5

--- scripts/pyenv_init.sh
+++ scripts/pyenv_init.sh
@@ -3,8 +3,8 @@
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 cd ~/.pyenv/plugins
 git clone git://github.com/yyuu/pyenv-virtualenv.git
-pyenv install 3.6.3
-pyenv global 3.6.3
+pyenv install 3.6.5
+pyenv global 3.6.5
 source ~/.bashrc
 
 pip install --upgrade pip
