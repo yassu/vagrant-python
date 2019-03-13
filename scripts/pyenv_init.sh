b5d5c9f9a89ee880d4e9dc727dc5dd1115aef7b7
yassu <yassu0320.dev@gmail.com>
2019-03-13 23:48:25 +0900

    Update python version

--- scripts/pyenv_init.sh
+++ scripts/pyenv_init.sh
@@ -3,8 +3,8 @@
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 cd ~/.pyenv/plugins
 git clone git://github.com/yyuu/pyenv-virtualenv.git
-pyenv install 3.6.5
-pyenv global 3.6.5
+pyenv install 3.7.2
+pyenv global 3.7.2
 source ~/.bashrc
 
 pip install --upgrade pip
