18d7a5939c03a79fa4d65b941067555e1ea087ca
yassu <yassu0320.dev@gmail.com>
2019-03-28 02:51:27 +0900

    Install vim

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,3 +14,11 @@ yum install -y gcc gcc-c++ make git openssl-devel bzip2-devel zlib-devel \
     readline-devel sqlite-devel bzip2 sqlite zlib-devel bzip2 bzip2-devel \
     readline-devel sqlite sqlite-devel openssl-devel libffi-devel swig
 
+# install vim
+(
+    cd /tmp/ &&
+    git clone https://github.com/vim/vim.git &&
+    cd vim/
+    make
+    make install
+)
